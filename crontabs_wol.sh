56f2277818ca865814a2468bc9d82080b28ba4c3
Mowei <tso88206@gmail.com>
2024-07-31 09:20:24 +0800

    Update crontabs_wol.sh

--- crontabs_wol.sh
+++ crontabs_wol.sh
@@ -4,7 +4,8 @@ source_url="https://raw.githubusercontent.com/Mowei/wol/main/list"
 content=$(curl -k -s $source_url)
 
 if [ -n "$content" ]; then
-  IFS=',' read -r date mac <<< "$content"
+  date=$(echo $content | cut -d',' -f1)
+  mac=$(echo $content | cut -d',' -f2)
   if [ "$date" == "$(date +%Y/%m/%d)" ]; then
     curl -k -X GET "https://127.0.0.1:8443/wol_action.asp?dstmac=$mac"
   fi
