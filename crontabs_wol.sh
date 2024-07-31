ae08a6624d735d9b7c2e33391685ccd9ff4fe566
Mowei <tso88206@gmail.com>
2024-07-31 08:55:16 +0800

    Create crontabs_wol.sh

---
+++ crontabs_wol.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source_url="https://raw.githubusercontent.com/Mowei/wol/main/list"
+content=$(curl -k -s $source_url)
+
+if [ -n "$content" ]; then
+  IFS=',' read -r date mac <<< "$content"
+  if [ "$date" == "$(date +%Y/%m/%d)" ]; then
+    curl -k -X GET "https://127.0.0.1:8443/wol_action.asp?dstmac=$mac"
+  fi
+fi
+EOF
